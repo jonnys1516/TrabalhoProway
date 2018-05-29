b37e962f3bc60e7c4bc01fc09ab507c2eca524fb
Nicolas Cunha <Nicolas Cunha@DESKTOP-3IIDAGQ>
2018-05-29 20:01:43 -0300

    ...

--- src/principal/TrabalhoProWay.java
+++ src/principal/TrabalhoProWay.java
@@ -5,7 +5,6 @@ import formularios.TelaLogin;
 public class TrabalhoProWay {
 
     public static void main(String[] args) {
-        // TODO implementar insereFuncionario
         // TODO implementar geraTxt
         // TODO implementar relatório -> .txt
         TelaLogin telaLogin = new TelaLogin();
