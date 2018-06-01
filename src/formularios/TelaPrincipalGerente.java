13b853c30c53c5270b6b57e410f66facbcd6fb6f
104936 <104936@SALA412.proway.treina>
2018-06-01 11:46:22 -0300

    ...

--- src/formularios/TelaPrincipalGerente.java
+++ src/formularios/TelaPrincipalGerente.java
@@ -24,6 +24,12 @@ public class TelaPrincipalGerente extends javax.swing.JFrame {
         buttonEncerrarSessao = new javax.swing.JButton();
         buttonSair = new javax.swing.JButton();
         buttonVisualizarHistorico = new javax.swing.JButton();
+        labelFuncionario = new javax.swing.JLabel();
+        labelProduto = new javax.swing.JLabel();
+        labelExibicao = new javax.swing.JLabel();
+        buttonRemoverFuncionario = new javax.swing.JButton();
+        buttonAtualizarFuncionario = new javax.swing.JButton();
+        buttonVisualizarFuncionarios = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Principal: Gerente");
@@ -31,7 +37,7 @@ public class TelaPrincipalGerente extends javax.swing.JFrame {
 
         buttonCadastrarFuncionario.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         buttonCadastrarFuncionario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/botoes/funcionario.png"))); // NOI18N
-        buttonCadastrarFuncionario.setText("Cadastrar Funcionário");
+        buttonCadastrarFuncionario.setText("Adicionar");
         buttonCadastrarFuncionario.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 buttonCadastrarFuncionarioActionPerformed(evt);
@@ -40,7 +46,7 @@ public class TelaPrincipalGerente extends javax.swing.JFrame {
 
         buttonCadastrarProduto.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         buttonCadastrarProduto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/botoes/produto.png"))); // NOI18N
-        buttonCadastrarProduto.setText("Cadastrar Produto");
+        buttonCadastrarProduto.setText("Adicionar");
         buttonCadastrarProduto.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 buttonCadastrarProdutoActionPerformed(evt);
@@ -49,7 +55,7 @@ public class TelaPrincipalGerente extends javax.swing.JFrame {
 
         buttonRemoverProduto.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         buttonRemoverProduto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/botoes/remover.png"))); // NOI18N
-        buttonRemoverProduto.setText("Remover Produto");
+        buttonRemoverProduto.setText("Remover");
         buttonRemoverProduto.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 buttonRemoverProdutoActionPerformed(evt);
@@ -66,8 +72,8 @@ public class TelaPrincipalGerente extends javax.swing.JFrame {
         });
 
         buttonAlterarProduto.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        buttonAlterarProduto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/botoes/alterar2.png"))); // NOI18N
-        buttonAlterarProduto.setText("Alterar Produto");
+        buttonAlterarProduto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/botoes/atualizar.png"))); // NOI18N
+        buttonAlterarProduto.setText("Atualizar");
         buttonAlterarProduto.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 buttonAlterarProdutoActionPerformed(evt);
@@ -101,43 +107,111 @@ public class TelaPrincipalGerente extends javax.swing.JFrame {
             }
         });
 
+        labelFuncionario.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        labelFuncionario.setText("FUNCIONÁRIO");
+
+        labelProduto.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        labelProduto.setText("PRODUTO");
+
+        labelExibicao.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        labelExibicao.setText("EXIBIÇÃO");
+
+        buttonRemoverFuncionario.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        buttonRemoverFuncionario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/botoes/funcionario.png"))); // NOI18N
+        buttonRemoverFuncionario.setText("Remover");
+        buttonRemoverFuncionario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonRemoverFuncionarioActionPerformed(evt);
+            }
+        });
+
+        buttonAtualizarFuncionario.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        buttonAtualizarFuncionario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/botoes/funcionario.png"))); // NOI18N
+        buttonAtualizarFuncionario.setText("Atualizar");
+        buttonAtualizarFuncionario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonAtualizarFuncionarioActionPerformed(evt);
+            }
+        });
+
+        buttonVisualizarFuncionarios.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        buttonVisualizarFuncionarios.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/botoes/equipe.png"))); // NOI18N
+        buttonVisualizarFuncionarios.setText("Visualizar Funcionários");
+        buttonVisualizarFuncionarios.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonVisualizarFuncionariosActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(72, 72, 72)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(buttonVisualizarEstoque, javax.swing.GroupLayout.DEFAULT_SIZE, 210, Short.MAX_VALUE)
-                    .addComponent(buttonCadastrarFuncionario, javax.swing.GroupLayout.DEFAULT_SIZE, 210, Short.MAX_VALUE)
-                    .addComponent(buttonVisualizarHistorico, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(buttonCadastrarProduto, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(buttonAlterarProduto, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(buttonRemoverProduto, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(buttonEncerrarSessao, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(buttonSair, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(buttonSair, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(buttonEncerrarSessao, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(buttonCadastrarFuncionario, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(buttonRemoverFuncionario, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(buttonAtualizarFuncionario, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(102, 102, 102)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(buttonCadastrarProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 130, Short.MAX_VALUE)
+                                .addComponent(buttonVisualizarHistorico, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(buttonRemoverProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(buttonVisualizarFuncionarios, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(buttonAlterarProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(buttonVisualizarEstoque, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(65, 65, 65)
+                .addComponent(labelFuncionario)
+                .addGap(221, 221, 221)
+                .addComponent(labelProduto)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(labelExibicao)
                 .addGap(72, 72, 72))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addComponent(buttonCadastrarFuncionario, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(29, 29, 29)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(labelFuncionario)
+                    .addComponent(labelProduto)
+                    .addComponent(labelExibicao))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(buttonCadastrarFuncionario, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(buttonCadastrarProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(buttonVisualizarHistorico, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(buttonVisualizarEstoque, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(buttonAlterarProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(buttonAtualizarFuncionario, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(buttonVisualizarEstoque, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(buttonVisualizarHistorico, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(buttonCadastrarProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(buttonAlterarProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(buttonRemoverProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(buttonRemoverProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(buttonRemoverFuncionario, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(buttonVisualizarFuncionarios, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(112, 112, 112)
                 .addComponent(buttonEncerrarSessao, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(buttonSair, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(36, 36, 36))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -211,16 +285,34 @@ public class TelaPrincipalGerente extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_buttonSairActionPerformed
 
+    private void buttonRemoverFuncionarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonRemoverFuncionarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_buttonRemoverFuncionarioActionPerformed
+
+    private void buttonAtualizarFuncionarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonAtualizarFuncionarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_buttonAtualizarFuncionarioActionPerformed
+
+    private void buttonVisualizarFuncionariosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonVisualizarFuncionariosActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_buttonVisualizarFuncionariosActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton buttonAlterarProduto;
+    private javax.swing.JButton buttonAtualizarFuncionario;
     private javax.swing.JButton buttonCadastrarFuncionario;
     private javax.swing.JButton buttonCadastrarProduto;
     private javax.swing.JButton buttonEncerrarSessao;
+    private javax.swing.JButton buttonRemoverFuncionario;
     private javax.swing.JButton buttonRemoverProduto;
     private javax.swing.JButton buttonSair;
     private javax.swing.JButton buttonVisualizarEstoque;
+    private javax.swing.JButton buttonVisualizarFuncionarios;
     private javax.swing.JButton buttonVisualizarHistorico;
+    private javax.swing.JLabel labelExibicao;
+    private javax.swing.JLabel labelFuncionario;
+    private javax.swing.JLabel labelProduto;
     // End of variables declaration//GEN-END:variables
 
 }
